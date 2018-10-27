6ce2bdf88a6d1f1b8d2a139509e32f41e6729bef
Sai Bhargav Yalamanchi <syalaman@matrix.ml.cmu.edu>
2018-10-27 09:40:48 -0400

    pkl_data qt

--- models/unrolledplan/pkl_data.py
+++ models/unrolledplan/pkl_data.py
@@ -23,15 +23,21 @@ def main():
     dirs = [_dir for _dir in dirs if os.path.isdir(_dir)]
 
     txt_data = []
+    qt_txt_data = []
     img_data = []
     lens = []
     for _dir in dirs:
-        txt_file = glob.glob(os.path.join(_dir, '*txt'))[0]
+        txt_file, qt_txt_file = glob.glob(os.path.join(_dir, '*txt'))
+        if 'qt.txt' in txt_file:
+            txt_file, qt_txt_file = qt_txt_file, txt_file
         img_files = glob.glob(os.path.join(_dir, '*p'))
         img_files = sorted(img_files, key=lambda x: int(x.split(os.path.sep)[-1].split(".")[0]))
         f = open(txt_file, 'r')
+        g = open(qt_txt_file, 'r')
         _txt_data = [line.strip().split() for line in f]#[1:]
+        _qt_txt_data = [line.strip().split() for line in g]#[1:]
         f.close()
+        g.close()
         _img_data = []
         for _img_file in img_files:
             img = pickle.load(open(_img_file, 'rb'))
@@ -39,18 +45,21 @@ def main():
             #img = plt.imread(_img_file)
             _img_data.append(np.array(img, dtype=np.float32)[:, :, :3]/255.)
 
-        if len(_txt_data) != len(_img_data):
-            _txt_data = _txt_data[1:]
+        #if len(_txt_data) != len(_img_data):
+        #    _txt_data = _txt_data[1:]
         assert len(_txt_data) == len(_img_data), "{}_{}".format(len(_txt_data), len(_img_data))
+        assert len(_qt_txt_data) == len(_img_data), "{}_{}".format(len(_qt_txt_data), len(_img_data))
         _txt_data = np.array(_txt_data, dtype=np.float32)
+        _qt_txt_data = np.array(_qt_txt_data, dtype=np.float32)
 
         txt_data.append(_txt_data)
+        qt_txt_data.append(_qt_txt_data)
         img_data.append(_img_data)
         lens.append(len(_img_data))
 
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
-    pickle.dump([txt_data, img_data, lens],
+    pickle.dump([txt_data, qt_txt_data, img_data, lens],
         open(os.path.join(args.save_dir,
         args.path_prefix.split(os.path.sep)[-1].split('*')[0]+'.p'),
         'wb'), protocol=2)
